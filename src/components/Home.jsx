c7970cbe1e6a18971e25537f5fffea7e6378f945
Manju Gurung <43616040+manju0912@users.noreply.github.com>
2026-05-16 14:17:03 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,7 @@ const Home = () => {
   return (
     <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:py-20 md:px-12 lg:px-24 lg:py-0 overflow-hidden'>
       <div className='lg:flex lg:justify-between lg:gap-4 relative'>
-        <div className='sticky top-10 w-full'>
-          <Header />
-        </div>
+        <Header />
         <Main />
       </div>
     </div>
