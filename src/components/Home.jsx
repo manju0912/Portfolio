c1d971ce6b3368e67ea48e02ebef0ce8a1f00c52
Manju Gurung <43616040+manju0912@users.noreply.github.com>
2026-05-16 14:13:06 +0530

    Make Header component sticky in Home.jsx
    Made the Header component sticky within the Home component layout.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ const Home = () => {
 
   return (
     <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:py-20 md:px-12 lg:px-24 lg:py-0 overflow-hidden'>
-      <div className='lg:flex lg:justify-between lg:gap-4'>
-        <Header />
+      <div className='lg:flex lg:justify-between lg:gap-4 relative'>
+        <div className='sticky top-10'>
+          <Header />
+        </div>
         <Main />
       </div>
     </div>
