8605ee6c99bbb64cb1dcff29e1647b3ca75d4e57
Manju Gurung <43616040+manju0912@users.noreply.github.com>
2026-05-16 14:18:31 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Main from './Main';
 const Home = () => {
 
   return (
-    <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:py-20 md:px-12 lg:px-24 lg:py-0 overflow-hidden'>
+    <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:py-20 md:px-12 lg:px-24 lg:py-0'>
       <div className='lg:flex lg:justify-between lg:gap-4 relative'>
         <Header />
         <Main />
